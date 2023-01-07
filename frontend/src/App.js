cd0a4a87e311b37f8f2eb1903ffb66b2c98509e0
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-07 15:37:46 -0800

    Firebase integrated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 
 function App() {
 
-  console.log(process.env.REACT_APP_FIREBASE_API_KEY);
   return (
     <div className="App">
       <header className="App-header">
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -4,13 +4,14 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: REACT_APP_FIREBASE_DATABASE_URL,
-    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyBNAjXR0N4EbQzSXVvOoi-4yrQKbGLWy_k",
+    authDomain: "qds-2023.firebaseapp.com",
+    projectId: "qds-2023",
+    storageBucket: "qds-2023.appspot.com",
+    messagingSenderId: "259392948048",
+    appId: "1:259392948048:web:12b91c5a60f2233a920e55"
 };
 // Initialize Firebase
 
