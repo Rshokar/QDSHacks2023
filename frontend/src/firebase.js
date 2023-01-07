794ccb7cfa8f9191fa4da91af9c039476aaf35c8
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-07 15:46:36 -0800

    Added ENVS to firebase

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,22 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 // Your web app's Firebase configuration
 
+const {
+    REACT_APP_FIREBASE_API_KEY,
+    REACT_APP_FIREBASE_AUTH_DOMAIN,
+    REACT_APP_FIREBASE_PROJECT_ID,
+    REACT_APP_FIREBASE_STORAGE_BUCKET,
+    REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    REACT_APP_FIREBASE_APP_ID
+} = process.env;
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBNAjXR0N4EbQzSXVvOoi-4yrQKbGLWy_k",
-    authDomain: "qds-2023.firebaseapp.com",
-    projectId: "qds-2023",
-    storageBucket: "qds-2023.appspot.com",
-    messagingSenderId: "259392948048",
-    appId: "1:259392948048:web:12b91c5a60f2233a920e55"
+    apiKey: REACT_APP_FIREBASE_API_KEY,
+    authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: REACT_APP_FIREBASE_APP_ID
 };
 // Initialize Firebase
 
