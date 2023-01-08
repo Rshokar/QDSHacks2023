3cb23887d6f8e989e9c18ad1f0bf7a74ec2177df
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-07 17:00:26 -0800

    Pages, Analytics, Insights

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
-import Page from './components/Page/Page';
+
 import Layout from './components/Layout/Layout';
+import Analytics from './features/Analytics/Analytics';
+import Insights from './features/Insights/Insights';
+import Page from './components/Page/Page';
 
 function App() {
-
   return (
     <div className="App">
       <Layout>
         <NavBar />
+        <Page>
+          <Analytics />
+        </Page>
       </Layout>
     </div>
   );
