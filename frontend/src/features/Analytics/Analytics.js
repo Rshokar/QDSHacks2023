c1bebbb5d401380c3ea4e0a110135e3c69e1f007
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-08 11:07:02 -0800

    Integration

--- frontend/src/features/Analytics/Analytics.js
+++ frontend/src/features/Analytics/Analytics.js
@@ -20,21 +20,116 @@ import FilterDropdown from '../../components/FilterDropdown';
 
 import { Container } from './styled';
 import { filterOpts, kpi, truckIDs, testData } from './constants';
+import Controller from '../../controllers/Controller';
 
 const Analytics = () => {
+    const con = new Controller();
     const [dateFilter, setDateFilter] = useState('');
     const [filter, setFilter] = useState(filterOpts[0]);
     const [id, setId] = useState(1);
     const [date, setDate] = useState(null);
     const [globalData, setGlobalData] = useState(null);
-    const [localData, setLocalData] = useState(null);
+    const [localData, setLocalData] = useState(testData.kpi);
+
+    useEffect(() => {
+        // TODO:
+        //      -> Query DB for globalData await con.analyticsOverall(id)
+        //      -> Will also need globalData (over 7 days) for truck ID 1 at this point (default)
+        //          -> setLocalData to result
+
+        const run = async () => {
+            console.log(id);
+            const doc = await con.getAnalyticsOverall(parseFloat(id));
+            const docs = await con.getAnalytics(parseFloat(id));
+
+            console.log("Analytics overall", doc);
+            console.log("Analytics", docs);
+
+            const result = {
+                worstRoute: {
+                    shovel: doc[0].worst_shovel_id,
+                    dump: doc[0].worst_dump_id,
+                    avg: doc[0].worst_efficiency,
+                },
+                bestRoute: {
+                    shovel: doc[0].best_shovel_id,
+                    dump: doc[0].best_dump_id,
+                    avg: doc[0].best_efficiency,
+                },
+                kpi: [
+                    {
+                        label: "efficiency",
+                        value: round(docs[0].Efficiency),
+                    },
+                    {
+                        label: "time per round",
+                        value: round(docs[0].time),
+                    },
+                    {
+                        label: "load per round",
+                        value: round(docs[0].payload),
+                    },
+                    {
+                        label: "fuel rate",
+                        value: round(docs[0].avg_fuel),
+                    }
+                ],
+            }
+
+            console.log(result);
+            setLocalData(result);
+        }
+        run();
+
+    }, [id]);
+
+
 
     useEffect(() => {
         setDate(dateFilter ? dayjs(dateFilter) : null);
+        const run = async () => {
+            const docs = await con.getAnalyticsSummary(dateFilter || "overall");
+            console.log(docs);
+
+            const result = [
+                {
+                    label: "efficiency",
+                    value: round(docs.efficiency.min),
+                },
+                {
+                    label: "time per round",
+                    value: round(docs.time.min),
+                },
+                {
+                    label: "load per round",
+                    value: round(docs.payload.min),
+                },
+                {
+                    label: "fuel rate",
+                    value: round(docs.fuel_rate.min),
+                }
+            ]
+
+            setGlobalData(result);
+        }
+
+        run();
     }, [dateFilter]);
 
     useEffect(() => {
-    }, [filter, dateFilter]);
+        // Filter igonore
+
+        // id truck_id
+
+        // dateFilter date
+
+        const run = async () => {
+            const documents = await con.getAnalytics(id, dateFilter);
+            console.log(documents);
+        }
+
+        run();
+    }, [filter, dateFilter, id]);
 
     const renderGlobalStats = (data) => data.map((d) => {
         return (
@@ -43,10 +138,13 @@ const Analytics = () => {
                     title={d.label}
                     value={d.value}
                     unit={kpi[d.label]["unitLong"] || kpi[d.label]["unit"]}
-                    icon={kpi[d.label]["icon"]}/>
+                    icon={kpi[d.label]["icon"]} />
             </Col>
         );
     });
+
+    const round = (num) => Math.round(num * 100) / 100;
+
 
     return (
         <Container>
@@ -63,7 +161,7 @@ const Analytics = () => {
                         </div>
                     }
                 </Col>
-                
+
                 <Col span={12}>
                     <Row gutter={16}>
                         {renderGlobalStats(globalData || testData.kpi)}
@@ -80,11 +178,11 @@ const Analytics = () => {
                         icon={<Icon component={Filter} />}
                     />
                     {filter === filterOpts[0]
-                        ? <FilterDropdown text="Truck ID" options={truckIDs} icon={<Icon component={Truck}/>}/>
+                        ? <FilterDropdown onSelectionChanged={(s) => setId(s)} text="Truck ID" options={truckIDs} icon={<Icon component={Truck} />} />
                         : <FilterDropdown text="Type ID" />}
                 </Col>
                 <Col span={12}>
-                    <OverviewBar />
+                    <OverviewBar data={localData?.kpi} />
                 </Col>
             </Row>
 
@@ -101,25 +199,25 @@ const Analytics = () => {
 
                     <Row gutter={16} className="lower">
                         <Col span={12}>
-                            <RouteCard 
+                            <RouteCard
                                 title="best route"
                                 route={testData.bestRoute}
                                 icon={<CheckCircleOutlined />}
                                 color="#198754"
                             />
                         </Col>
-                        
+
                         <Col span={12}>
                             <RouteCard
                                 title="worst route"
                                 route={testData.worstRoute}
-                                icon={<CloseCircleOutlined/>}
+                                icon={<CloseCircleOutlined />}
                                 color="#dc3545"
                             />
                         </Col>
                     </Row>
 
-                    
+
                 </Col>
             </Row>
         </Container>
