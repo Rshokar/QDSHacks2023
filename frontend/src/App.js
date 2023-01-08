f13e3ebd5f1beb555f687cf6cd48070a5d6cd7cf
Ravinder Shokar <Ravindershokar@hotmaiil.com>
2023-01-07 20:32:19 -0800

    Updated firebase

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,17 @@ import Insights from './features/Insights/Insights';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
-      <Layout>
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<Analytics />} />
-          <Route exact path="/insights" element={<Insights />} />
-        </Routes>
-      </Layout>
+        <Layout>
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<Analytics />} />
+            <Route exact path="/insights" element={<Insights />} />
+          </Routes>
+        </Layout>
       </Router>
 
     </div>
